ff118d769c05a67a1caa1995d39e53017cdcf8a5
apet2929 <realandrewpetersen@gmail.com>
2024-11-15 17:16:04 -0500

    Movement

--- scenes/worm.gd
+++ scenes/worm.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var move_ready = false
+const MOVE_TIMER = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+	
+func move(direction) -> void:
+	if move_ready:
+		start_move(direction)
+		move_ready = false
+		await get_tree().create_timer(MOVE_TIMER).timeout
+		move_ready = true
+	
+	
+func start_move(direction):
 	pass
