5da4844e7f047d35e0519887190481885da6ba20
LaDogo1234 <drorkeren212@gmail.com>
2024-11-16 01:01:23 +0200

    simple worm movement

--- scenes/worm.gd
+++ scenes/worm.gd
@@ -1,24 +1,38 @@
-extends Node3D
+extends CharacterBody3D
 
-var move_ready = false
+var move_ready = true
 const MOVE_TIMER = 1.0
+const GRID_SIZE = 1.0
+const PUSH_FORCE = 1.0
+
+@onready var bottom_ray: RayCast3D = $bottomRay
+@onready var top_ray: RayCast3D = $topRay
+@onready var left_ray: RayCast3D = $leftRay
+@onready var right_ray: RayCast3D = $rightRay
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var move_ready = true
+	move_ready = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	move()
+	move_and_slide()
 	
-func move(direction) -> void:
+func move() -> void:
 	if move_ready:
-		start_move(direction)
-		move_ready = false
-		await get_tree().create_timer(MOVE_TIMER).timeout
-		move_ready = true
+		var dir = Input.get_vector("forward", "backward", "right", "left")
+		if dir != Vector2(0,0) and abs(dir.x) != abs(dir.y):
+			start_move(dir)
+			move_ready = false
+			await get_tree().create_timer(MOVE_TIMER).timeout
+			self.velocity = Vector3(0,0,0)
+			
+			self.position.x = round(self.position.x)
+			self.position.z = round(self.position.z)
+			move_ready = true
 	
 	
 func start_move(direction):
-	pass
+	self.velocity = Vector3(direction.x, 0, direction.y) * PUSH_FORCE
