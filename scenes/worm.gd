f71ece640f7332a2233ab038ae85364fd725abb3
apet2929 <realandrewpetersen@gmail.com>
2024-11-15 17:07:58 -0500

    start prototyping

---
+++ scenes/worm.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var move_ready = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var move_ready = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
