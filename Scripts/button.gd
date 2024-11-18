346f19795568afafb58d42b46bc08a67e87a04ae
apet2929 <realandrewpetersen@gmail.com>
2024-11-18 15:55:19 -0500

    add new stuff to scene

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -8,6 +8,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
 
 
 # two spheres for head and tail
