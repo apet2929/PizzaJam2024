aaa2db85cb6c22f9f0f7aac34ade0b53f7ef9c2b
LaDogo1234 <drorkeren212@gmail.com>
2024-11-23 22:20:49 +0200

    New level and finsih sfx

--- Scripts/finish.gd
+++ Scripts/finish.gd
@@ -2,8 +2,10 @@ extends Area3D
 
 
 const WORM_SCRIPT = preload("res://Scripts/new_worm.gd")
+@onready var win_sfx: AudioStreamPlayer3D = $win_sfx
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.get_script() == WORM_SCRIPT:
 		print("foo bar")
 		EventBus.level_finished.emit(body)
+		win_sfx.play()
