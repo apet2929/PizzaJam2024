2db06e4a08b61c8776a0f4dc670335595fbb7087
apet2929 <realandrewpetersen@gmail.com>
2024-11-23 17:22:56 -0500

    smaller spikes

---
+++ Scripts/Levels/level_13.gd
@@ -0,0 +1,22 @@
+extends LevelBase
+
+var next_level_scene = "res://scenes/Levels/Level14.tscn"
+
+func _ready() -> void:
+	super._ready()
+
+func _process(delta) -> void:
+	super._process(delta)
+
+func next_level(_body):
+	super._next_level(next_level_scene)
+	
+func init_signals():
+	EventBus.connect("button_pressed", self._on_button_pressed)
+	EventBus.connect("button_unpressed", self._on_button_unpressed)
+
+func _on_button_pressed(button, body) -> void:
+	pass
+
+func _on_button_unpressed(button, body) -> void:
+	pass
