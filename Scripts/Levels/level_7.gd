96030af2cc6662a3ecd0ef3a5d724f4b46236e5b
apet2929 <realandrewpetersen@gmail.com>
2024-11-23 20:55:41 -0500

    level 7 rework

--- Scripts/Levels/level_7.gd
+++ Scripts/Levels/level_7.gd
@@ -1,73 +1,43 @@
-extends Node3D
+extends LevelBase
 
-@export var next_lvl = "res://scenes/Levels/Level3.tscn"
-const WORM_SCRIPT = preload("res://Scripts/new_worm.gd")
-# Contains all the setup/interaction logic specific to this level
-
-var dropping = false
-var worm_vel_y = 0
-const GRAVITY = -9
-var worm_initial_y
+var next_level_scene = "res://scenes/Levels/Level8.tscn"
+var fence5_open = false
 
 func _ready() -> void:
-	$SceneTransition.load_in()
-	$SceneTransition.anim_done.connect(self.start_level)
-	$Hand.dropped.connect(self.drop_worm)
-	EventBus.connect("level_finished", next_level)
-	worm_initial_y = $Worm.global_position.y
-	$Worm.global_position.y = $Hand.global_position.y - 5
-	for worm in get_tree().get_nodes_in_group("head"):
-		worm.disabled = true
+	super._ready()
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("retry"):
-		restart_level()
-	if dropping:
-		drop_worm_update(delta)
+func _process(delta) -> void:
+	super._process(delta)
+	var should_open_fence3 = true
+	for plate in get_tree().get_nodes_in_group("fence3_pps"):
+		if !plate.is_pressed:
+			should_open_fence3 = false
+	if should_open_fence3 and not $Fence3.open:
+		$Fence3.open_fence()
 
-func next_level():
-	$SceneTransition.load_out(next_lvl)
-
-func start_level():
-	await get_tree().create_timer(0.3).timeout
-	$Hand.unpinch()
-
-func restart_level():
-	EventBus.restart_count += 1
-	get_tree().change_scene_to_file(self.scene_file_path)
-
-func drop_worm():
-	dropping = true
+func next_level(_body):
+	super._next_level(next_level_scene)
 	
-func drop_worm_update(delta):
-	worm_vel_y += GRAVITY * delta
-	$Worm.global_position.y += worm_vel_y * delta
-	if abs($Worm.global_position.y - worm_initial_y) < 0.2:
-		dropping = false
-		$Worm.global_position.y = worm_initial_y
-		await get_tree().create_timer(0.1).timeout
-		for worm in get_tree().get_nodes_in_group("head"):
-			worm.disabled = false
+func init_signals():
+	EventBus.connect("button_pressed", self._on_button_pressed)
+	EventBus.connect("button_unpressed", self._on_button_unpressed)
 
-func is_worm(body):
-	return body.get_script() == WORM_SCRIPT
-
-func _on_button_small_button_pressed(button_id, body) -> void:
-	$Fence.open_fence()
-
-func _on_button_small_button_unpressed(button_id, body) -> void:
-	if $Fence.open:
-		$Fence.close_fence()
-
-func _on_pressure_pad_body_entered(body: Node3D) -> void:
-	if is_worm(body):
+func _on_button_pressed(button, body) -> void:
+	if button == $ButtonSmall:
 		$Guillotine.drop()
-		
-func _on_pressure_pad_pressed(pressure_pad, body) -> void:
-	if pressure_pad == $PressurePad:
-		$Guillotine.drop()
+	if button == $ButtonSmall2:
+		fence5_open = true
+		$Fence5.open_fence()
+	elif button == $PressurePad3:
+		$Fence5.open_fence()
+		$Fence6.close_fence()
+	elif button == $PressurePad:
+		$Fence4.open_fence()
 
-func _on_pressure_pad_unpressed(pressure_pad, body) -> void:
-	if pressure_pad == $PressurePad:
-		$Guillotine.undrop()
-		
+func _on_button_unpressed(button, body) -> void:
+	if button == $PressurePad3:
+		if !fence5_open:
+			$Fence5.close_fence()
+		$Fence6.open_fence()
+	elif button == $PressurePad:
+		$Fence4.close_fence()
